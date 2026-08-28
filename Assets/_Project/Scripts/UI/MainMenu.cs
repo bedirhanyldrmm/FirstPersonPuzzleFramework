ac53c83a49fe1ed14761a6fdff09d3ec1f2d8185
Bedirhan Yıldırım <93772777+bedirhanyldrmm@users.noreply.github.com>
2026-08-28 18:28:27 +0300

    feat: polish pause menu UI

--- Assets/_Project/Scripts/UI/MainMenu.cs
+++ Assets/_Project/Scripts/UI/MainMenu.cs
@@ -13,8 +13,6 @@ public class MainMenu : MonoBehaviour
     public void LoadGame()
     {
         PlayerPrefs.SetInt("LoadGameRequested", 1);
-        PlayerPrefs.Save();
-
         SceneManager.LoadScene(gameSceneName);
     }
 
